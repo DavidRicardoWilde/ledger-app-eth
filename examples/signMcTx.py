08a4cfd0c8bc2b4296209b0d518a94b5fc31dc1e
david <davidgluttongk@outlook.com>
2019-07-02 19:18:38 -0700

    add python file for test mc_tx

--- examples/signMcTx.py
+++ examples/signMcTx.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-*******************************************************************************
+********************************************************************************
 *   Ledger Ethereum App
 *   (c) 2016-2019 Ledger
 *
@@ -25,17 +25,19 @@ from decimal import Decimal
 import argparse
 import struct
 import binascii
-from ethBase import Transaction, UnsignedTransaction, unsigned_tx_from_tx
+from ethBaseMc import Transaction, UnsignedTransaction, unsigned_tx_from_tx
+#from ethBase import Transaction, UnsignedTransaction, unsigned_tx_from_tx
 from rlp import encode
 
 # Define here Chain_ID for EIP-155
+#CHAIN_ID = 0
 CHAIN_ID = 99
 
 try:
     from rlp.utils import decode_hex, encode_hex, str_to_bytes
 except:
     #Python3 hack import for pyethereum
-    from ethereum.utils import decode_hex, encode_hex, str_to_bytes
+    from chain3 import decode_hex, encode_hex, str_to_bytes
 
 def parse_bip32_path(path):
     if len(path) == 0:
@@ -59,6 +61,9 @@ parser.add_argument('--amount', help="Amount to send in ether", required=True)
 parser.add_argument('--to', help="Destination address", type=str, required=True)
 parser.add_argument('--path', help="BIP 32 path to sign with")
 parser.add_argument('--data', help="Data to add, hex encoded")
+parser.add_argument('--systemflag', help="always 0", default='0', type=int)
+parser.add_argument('--shardingflag', help="always 0", default='0', type=int)
+parser.add_argument('--via', help="always 0", default='0x00000000000000000000000000000000000000000', type=str)
 args = parser.parse_args()
 
 if args.path == None:
@@ -71,6 +76,9 @@ else:
 
 amount = Decimal(args.amount) * 10**18
 
+#print('check "to" ',decode_hex(args.to[2:]))
+#via_test="0000000000000000000000000000000000000000"
+
 tx = Transaction(
     nonce=int(args.nonce),
     gasprice=int(args.gasprice),
@@ -78,12 +86,19 @@ tx = Transaction(
     to=decode_hex(args.to[2:]),
     value=int(amount),
     data=args.data,
+    systemflag=0,
+    shardingflag=0,
+    via=decode_hex(args.to[2:]),
     v=CHAIN_ID,
     r=0,
     s=0
 )
 
 encodedTx = encode(tx, Transaction)
+#encoded = hex('f84309808504a817c80082520894f8558382014485843b9aca008382520880a0a0a0880de0b6b3a76400008080940000000000000000000000000000000000000000638080')
+#encodeStr = 'f84309808504a817c80082520894f8558382014485843b9aca008382520880a0a0a0880de0b6b3a76400008080940000000000000000000000000000000000000000638080'
+#encoded = bytearray(encodeStr)
+#print("test encode",encoded)
 print("encodedTx: ",binascii.hexlify(encodedTx))
 
 donglePath = parse_bip32_path(args.path)
@@ -98,18 +113,27 @@ result = dongle.exchange(bytes(apdu))
 # Needs to recover (main.c:1121)
 if (CHAIN_ID*2 + 35) + 1 > 255:
 	ecc_parity = result[0] - ((CHAIN_ID*2 + 35) % 256)
-	print("ecc_parity: ",ecc_parrity)
 	v = (CHAIN_ID*2 + 35) + ecc_parity
-	print("v1: ",v)
 else:
 	v = result[0]
-	print("v2: ",v)
 
-print("v: ",v)
+print('v: ',v)
+
 r = int(binascii.hexlify(result[1:1 + 32]), 16)
 s = int(binascii.hexlify(result[1 + 32: 1 + 32 + 32]), 16)
 
+#systemflag=0
+#shardingflag=0
+#via="00000000000000000000000000000000000000000"
+
+print('r: ',r)
+print('s: ',s)
+
 tx = Transaction(tx.nonce, tx.gasprice, tx.startgas,
-                 tx.to, tx.value, tx.data, v, r, s)
+                 tx.to, tx.value, tx.data, tx.systemflag, tx.shardingflag, tx.via, v, r, s)
+
+#tx = Transaction(tx.nonce, tx.gasprice, tx.startgas,
+#                 tx.to, tx.value, tx.data, v, r, s)
 
 print("Signed transaction", encode_hex(encode(tx)))
+
