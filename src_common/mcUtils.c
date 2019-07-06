e7c3c5333c025d8c456d60846be670cec637be4e
David Wilde <davidgluttongk@outlook.com>
2019-07-06 12:00:13 -0700

    update .

--- src_common/mcUtils.c
+++ src_common/mcUtils.c
@@ -164,8 +164,8 @@ void getMcAddressStringFromKey(cx_ecfp_public_key_t *publicKey, uint8_t *out,
     uint8_t hashAddress[32];
     cx_keccak_init(sha3Context, 256);
     cx_hash((cx_hash_t*)sha3Context, CX_LAST, publicKey->W + 1, 64, hashAddress);
-    getEtcAddressStringFromBinary(hashAddress + 12, out, sha3Context);
-    // getMcAddressStringFromBinary(hashAddress + 12, out, sha3Context);
+    // getEtcAddressStringFromBinary(hashAddress + 12, out, sha3Context);
+    getMcAddressStringFromBinary(hashAddress + 12, out, sha3Context);
 }
 
 
